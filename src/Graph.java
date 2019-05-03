8fce631519ddc12062c6d32ebdd48742dbb8fd2a
sunhaoru <sunhaoru@grinnell.edu*>
2019-05-03 09:50:03 -0500

    reachableForm

--- src/Graph.java
+++ src/Graph.java
@@ -22,11 +22,11 @@ public class Graph {
   // +-------+
 
   /*
-   * We implement our graphs using adjacency lists.  For each
-   * vertex, v, we store a list of edges from that vertex.
+   * We implement our graphs using adjacency lists. For each vertex, v, we store a list of edges
+   * from that vertex.
    *
-   * For convenience, you can refer to vertices by number or by
-   * name.  However, it is more efficient to refer to them by number.
+   * For convenience, you can refer to vertices by number or by name. However, it is more efficient
+   * to refer to them by number.
    */
 
   // +-----------+---------------------------------------------------
@@ -53,14 +53,12 @@ public class Graph {
   int numEdges;
 
   /**
-   * The vertices in the graph.  The edges from vertex v are
-   * stored in vertices[v].
+   * The vertices in the graph. The edges from vertex v are stored in vertices[v].
    */
   List<Edge>[] vertices;
 
   /**
-   * The names of the vertices.  The name of vertex v is stored
-   * in vertexNames[v].
+   * The names of the vertices. The name of vertex v is stored in vertexNames[v].
    */
   String[] vertexNames;
 
@@ -70,14 +68,12 @@ public class Graph {
   Queue<Integer> unusedVertices;
 
   /**
-   * The numbers of the vertices.  The vertex with name n is in
-   * vertexNumbers.get(n).
+   * The numbers of the vertices. The vertex with name n is in vertexNumbers.get(n).
    */
-  HashMap<String,Integer> vertexNumbers;
+  HashMap<String, Integer> vertexNumbers;
 
   /**
-   * The version of the graph.  (Essentially, the number of times
-   * we've modified the graph.)
+   * The version of the graph. (Essentially, the number of times we've modified the graph.)
    */
   long version;
 
@@ -99,7 +95,7 @@ public class Graph {
   public Graph(int initialCapacity) {
     this.vertices = (ArrayList<Edge>[]) new ArrayList[initialCapacity];
     this.vertexNames = new String[initialCapacity];
-    this.vertexNumbers = new HashMap<String,Integer>();
+    this.vertexNumbers = new HashMap<String, Integer>();
     this.unusedVertices = new LinkedList<Integer>();
     this.version = 0;
     for (int i = 0; i < this.vertices.length; i++) {
@@ -113,8 +109,8 @@ public class Graph {
   // +----------------------+
 
   /**
-   * Given a vertex number, get the corresponding vertex name.
-   * If there is no corresponding vertex name, returns null;
+   * Given a vertex number, get the corresponding vertex name. If there is no corresponding vertex
+   * name, returns null;
    */
   public String vertexName(int vertexNumber) {
     if ((vertexNumber < 0) || (vertexNumber >= vertexNames.length)) {
@@ -125,8 +121,8 @@ public class Graph {
   } // vertexName(int)
 
   /**
-   * Given a vertex name, get the corresponding vertex number.
-   * If there is no corresponding vertex number, returns -1.
+   * Given a vertex name, get the corresponding vertex number. If there is no corresponding vertex
+   * number, returns -1.
    */
   public int vertexNumber(String vertexName) {
     Integer result = vertexNumbers.get(vertexName);
@@ -159,7 +155,7 @@ public class Graph {
     } // for
     pen.println();
   } // dump()
-  
+
   /**
    * Get the number of edges.
    */
@@ -179,7 +175,7 @@ public class Graph {
    */
   public Iterator<Edge> edges() {
     return new Iterator<Edge>() {
-      // The position of the iterator 
+      // The position of the iterator
       int pos = 0;
       // The version number of the graph when this iterator was created
       long version = Graph.this.version;
@@ -219,10 +215,12 @@ public class Graph {
     if ((vertex < 0) || (vertex >= this.vertices.length)) {
       return new Iterator<Edge>() {
         long version = Graph.this.version;
+
         public boolean hasNext() {
           failFast(this.version);
           return false;
         } // hasNext()
+
         public Edge next() {
           failFast(this.version);
           throw new NoSuchElementException();
@@ -232,17 +230,17 @@ public class Graph {
       return new Iterator<Edge>() {
         // The version number of the graph when this iterator was created
         long version = Graph.this.version;
-        // The underlying iterator.  We wrap it so that the client
+        // The underlying iterator. We wrap it so that the client
         // cannot call the remove method.
         Iterator<Edge> edges = Graph.this.vertices[vertex].iterator();
 
         public boolean hasNext() {
-          failFast(this.version);
+          failFast(this.version);// concurrent
           return edges.hasNext();
         } // hasNext()
 
         public Edge next() {
-          failFast(this.version);
+          failFast(this.version);// concurrent
           return edges.next();
         } // next()
       }; // new Iterator<Edge>
@@ -296,8 +294,8 @@ public class Graph {
   // +----------+
 
   /**
-   * Add an edge between two vertices.  If the edge already exists,
-   * replace it.  If the vertices are invalid, throws an exception.
+   * Add an edge between two vertices. If the edge already exists, replace it. If the vertices are
+   * invalid, throws an exception.
    */
   public void addEdge(int from, int to, int weight) throws Exception {
     if (!validVertex(from) || !validVertex(to)) {
@@ -320,8 +318,8 @@ public class Graph {
   } // addEdge(int, int, int)
 
   /**
-   * Add an edge between two vertices.  If the edge already exists,
-   * replace it.  if the vertices are invalid, throws an exception.
+   * Add an edge between two vertices. If the edge already exists, replace it. if the vertices are
+   * invalid, throws an exception.
    */
   public void addEdge(String from, String to, int weight) throws Exception {
     addEdge(this.vertexNumber(from), this.vertexNumber(to), weight);
@@ -358,7 +356,7 @@ public class Graph {
   } // addVertex()
 
   /**
-   * Remove an edge.  If the edge does not exist, does nothing.
+   * Remove an edge. If the edge does not exist, does nothing.
    */
   public void removeEdge(int from, int to) {
     Iterator<Edge> ie = this.vertices[from].iterator();
@@ -374,14 +372,14 @@ public class Graph {
   } // removeEdge(int, int)
 
   /**
-   * Remove an edge.  If the edge does not exist, does nothing.
+   * Remove an edge. If the edge does not exist, does nothing.
    */
   public void removeEdge(String from, String to) {
     removeEdge(this.vertexNumber(from), this.vertexNumber(to));
   } // removeEdge(String, String)
 
   /**
-   * Remove a vertex.  If the vertex does not exist, does nothing.
+   * Remove a vertex. If the vertex does not exist, does nothing.
    */
   public void removeVertex(int vertex) {
     // Ignore pointless vertex numbers
@@ -414,16 +412,55 @@ public class Graph {
   } // removeVertex(int)
 
   /**
-   * Remove a vertex.  If the vertex does not exist, does nothing.
+   * Remove a vertex. If the vertex does not exist, does nothing.
    */
   public void removeVertex(String vertex) {
     this.removeVertex(this.vertexNumber(vertex));
   } // removeVertex(String)
 
+  /**
+   * Prints out a list of all the vertices reachable from a starting point.
+   * 
+   * @param pen
+   * @param vertex starting point
+   */
+  public void reachableFrom(PrintWriter pen, int vertex) {
+    // Help store printed vertex
+    ArrayList<Integer> reached = new ArrayList<Integer>();
+    reached.add(vertex);
+    // Create a queue
+    LinkedList<Edge> reach = helper(new LinkedList<Edge>(), vertex);
+    while (!reach.isEmpty()) {
+      Edge reachEdge = reach.remove();
+      if (!reached.contains(reachEdge.to())) {
+        reached.add(reachEdge.to());
+        // Do the print work
+        pen.println(
+            this.vertexName(reachEdge.to()) + " is reachable from " + this.vertexName(vertex));
+        reach = helper(reach, reachEdge.to());
+      } // if
+    } // while
+  }// reachableForm()
+
+  /**
+   * Helper procedure for reachableFrom
+   * 
+   * @param reachable
+   * @param vertex
+   * @return
+   */
+  public LinkedList<Edge> helper(LinkedList<Edge> reachable, int vertex) {
+    Iterator<Edge> ie = this.vertices[vertex].iterator();
+    while (ie.hasNext()) {
+      reachable.add(ie.next());
+    } // while
+    return reachable;
+  }
+
   // +-----------+---------------------------------------------------
   // | Utilities |
   // +-----------+
- 
+
   /**
    * Add a vertex name / vertex number pair.
    *
@@ -436,7 +473,7 @@ public class Graph {
     this.vertexNames[v] = name;
     return v;
   } // addVertex(String, int)
-    
+
   /**
    * Expand the necessary arrays.
    */
@@ -452,9 +489,8 @@ public class Graph {
   } // expand()
 
   /**
-   * Compare an expected version to the current version.  Die
-   * if they do not match.  (Used to implement the traditional
-   * "fail fast" policy for iterators.)
+   * Compare an expected version to the current version. Die if they do not match. (Used to
+   * implement the traditional "fail fast" policy for iterators.)
    */
   private void failFast(long expectedVersion) {
     if (this.version != expectedVersion) {
@@ -466,8 +502,7 @@ public class Graph {
    * Determine if a vertex is valid.
    */
   private boolean validVertex(int vertex) {
-    return ((vertex >= 0) && (vertex < this.vertices.length) &&
-        (this.vertexNames[vertex] != null));
+    return ((vertex >= 0) && (vertex < this.vertices.length) && (this.vertexNames[vertex] != null));
   } // validVertex
 
   /**
--- src/GraphExperiment.java
+++ src/GraphExperiment.java
@@ -12,50 +12,56 @@ public class GraphExperiment {
     g.addVertex("a");
     g.addVertex("b");
     g.addVertex("c");
+    // g.addVertex("d");
+    // g.addVertex("e");
     g.dump(pen);
+    // g.reachableFrom(pen, g.vertexNumber("a"));
 
     // Add a few edges
     g.addEdge("a", "b", 1);
     g.addEdge("a", "c", 2);
     g.addEdge("b", "c", 3);
     g.addEdge("b", "a", 4);
+    // g.addEdge("c", "d", 5);
+    // g.addEdge("d", "e", 6);
     g.dump(pen);
-    
+    g.reachableFrom(pen, g.vertexNumber("a"));
+
     // Remove a vertex
     g.removeVertex("b");
     g.dump(pen);
-    
+
     // Add another vertex
     g.addVertex("d");
     g.addEdge("a", "d", 5);
     g.addEdge("d", "a", 6);
     g.dump(pen);
-    
+
     // And another (hopefully, this will replace the old b)
     g.addVertex("e");
     g.addEdge("e", "a", 7);
     g.dump(pen);
-    
+
     // And another (hopefully, this will expand the graph again)
     g.addVertex("f");
     g.addEdge("c", "f", 8);
     g.addEdge("f", "c", 9);
     g.dump(pen);
-    
+
     // Add an invalid edge
     try {
-      g.addEdge("c",  "g", 0);
+      g.addEdge("c", "g", 0);
       pen.println("Surprisingly, added an edge from c to nonexistant g");
     } catch (Exception e) {
       pen.println("Correctly failed to add an edge from c to g.");
     }
-    
+
     // Add/replace a bunch of edges
     for (int i = 1; i <= 4; i++) {
-      g.addEdge(0,i,i*10);
+      g.addEdge(0, i, i * 10);
     } // for
     g.dump(pen);
-    
+
     // Remove an edge
     g.removeEdge("a", "c");
     g.dump(pen);
